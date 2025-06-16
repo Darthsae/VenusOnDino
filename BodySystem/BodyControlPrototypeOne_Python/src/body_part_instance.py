1500bfc37f287136ed0530eb8e9f0b7e12bfe4ba
Darthsae <sockmit2007@gmail.com>
2025-06-16 18:19:06 -0400

    Moo?

--- BodySystem/BodyControlPrototypeOne_Python/src/body_part_instance.py
+++ BodySystem/BodyControlPrototypeOne_Python/src/body_part_instance.py
@@ -1,5 +1,8 @@
 from .body_part_type import BodyPartType
+from .physics.connection import Connection
 
 class BodyPartInstance:
-    def __init__(self, type: BodyPartType):
+    def __init__(self, type: BodyPartType, name: str):
         self.type = type
+        self.name = name
+        self.connections: Connection
